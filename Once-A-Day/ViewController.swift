2228383945455ca042980cd2b7da1f0ce5646168
Jongho Lee <bazinga940@gmail.com>
2021-10-16 23:54:01 +0900

    First commit

---
+++ Once-A-Day/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Once-A-Day
+//
+//  Created by Jongho Lee on 2021/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
