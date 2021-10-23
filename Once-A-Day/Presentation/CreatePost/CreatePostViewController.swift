2acd06b154c00899fb0821066c81f7484f6fef64
Jongho Lee <bazinga940@gmail.com>
2021-10-23 20:05:12 +0900

    create post 생성

---
+++ Once-A-Day/Presentation/CreatePost/CreatePostViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreatePostViewController.swift
+//  Once-A-Day
+//
+//  Created by Jongho Lee on 2021/10/23.
+//
+
+import UIKit
+
+class CreatePostViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
